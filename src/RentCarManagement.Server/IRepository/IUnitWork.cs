dfa17f1973b11d23cd2ea139ef8c2a40aa7f19f7
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-05-16 22:01:02 +0200

    Created view of repository

---
+++ src/RentCarManagement.Server/IRepository/IUnitWork.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.Domain;
+
+namespace RentCarManagement.IRepository;
+
+public interface IUnitWork : IDisposable
+{
+    Task Save(HttpContext httpContext);
+    IGenericRepository<Make> Make { get; }
+    IGenericRepository<Model> Models { get; }
+    IGenericRepository<Vehicle> Vehicles { get; }
+    IGenericRepository<Colour> Colours { get; }
+    IGenericRepository<Booking> Bookings { get; }
+    IGenericRepository<Customer> Customers { get; }
+}
