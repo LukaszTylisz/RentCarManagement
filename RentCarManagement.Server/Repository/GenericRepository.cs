89b7ee4c2b1fd3c0d490a43ca3a4794a32317611
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-05-19 21:32:58 +0200

    Creating repository

--- RentCarManagement.Server/Repository/GenericRepository.cs
+++ RentCarManagement.Server/Repository/GenericRepository.cs
@@ -1,6 +1,80 @@
-﻿namespace RentCarManagement.Server.Repository;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using RentCarManagement.Server.Data;
+using RentCarManagement.Server.IRepository;
 
-public class GenericRepository
+namespace RentCarManagement.Server.Repository;
+
+public class GenericRepository<T>(ApplicationDbContext dbContext, DbSet<T> db) : IGenericRepository<T> where T : class
 {
+    private readonly DbSet<T> _db = db;
     
+    public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null)
+    {
+        IQueryable<T> query = db;
+
+        if (expression != null)
+        {
+            query = query.Where(expression);
+        }
+
+        if (includes != null)
+        {
+            query = includes(query);
+        }
+
+        if (orderBy != null)
+        {
+            query = orderBy(query);
+        }
+
+        return await query
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<T> Get(Expression<Func<T, bool>> expression,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null)
+    {
+        IQueryable <T> query = db;
+
+        if (includes != null)
+        {
+            query = includes(query);
+        }
+
+        return await query
+            .AsNoTracking()
+            .FirstOrDefaultAsync(expression);
+    }
+
+    public async Task Insert(T entity)
+    {
+        await db.AddAsync(entity);
+    }
+
+    public async Task InsertRange(IEnumerable<T> entities)
+    {
+        await db.AddRangeAsync(entities);
+    }
+
+    public async Task Delete(int id)
+    {
+        var record = await db.FindAsync(id);
+        db.Remove(record);
+    }
+
+    public void DeleteRange(IEnumerable<T> entities)
+    {
+        db.RemoveRange(entities);
+    }
+
+    public void Update(T entity)
+    {
+        db.Attach(entity);
+        dbContext.Entry(entity).State = EntityState.Modified;
+    }
 }
--- RentCarManagement.Server/Repository/UnitOfWork.cs
+++ RentCarManagement.Server/Repository/UnitOfWork.cs
@@ -1,6 +1,51 @@
-﻿namespace RentCarManagement.Server.Repository;
+﻿using Microsoft.AspNetCore.Identity;
+using RentCarManagement.Server.Data;
+using RentCarManagement.Server.IRepository;
+using RentCarManagement.Server.Models;
+using Shared.Domain;
 
-public class UnitOfWork
+namespace RentCarManagement.Server.Repository;
+
+public class UnitOfWork(
+    ApplicationDbContext dbContext,
+    UserManager<ApplicationUser> userManager,
+    IGenericRepository<Make> makes,
+    IGenericRepository<Model> models,
+    IGenericRepository<Colour> colours,
+    IGenericRepository<Booking> bookings,
+    IGenericRepository<Customer> customers,
+    IGenericRepository<Vehicle> vehicles) : IUnitWork
 {
+    private IGenericRepository<Make> _makes = makes;
+    private IGenericRepository<Model> _models = models;
+    private IGenericRepository<Colour> _colours = colours;
+    private IGenericRepository<Booking> _bookings = bookings;
+    private IGenericRepository<Customer> _customers = customers;
+    private IGenericRepository<Vehicle> _vehicles = vehicles;
+
+    public void Dispose()
+    {
+        dbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public IGenericRepository<Make> Makes 
+        => makes ??= new GenericRepository<Make>(dbContext);
+    public IGenericRepository<Model> Models
+        => models ??= new GenericRepository<Model>(dbContext);
+    public IGenericRepository<Colour> Colours
+        => colours ??= new GenericRepository<Colour>(dbContext);
+    public IGenericRepository<Vehicle> Vehicles
+        => vehicles ??= new GenericRepository<Vehicle>(dbContext);
+    public IGenericRepository<Booking> Bookings
+        => bookings ??= new GenericRepository<Booking>(dbContext);
+    public IGenericRepository<Customer> Customers
+        => customers ??= new GenericRepository<Customer>(dbContext);
     
+    public Task Save(HttpContext httpContext)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IGenericRepository<Make> Make { get; }
 }
