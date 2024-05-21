e08227c80d144bd17a6275cf2d3e4ff62317ba0d
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-05-21 09:31:25 +0200

    Adding Client to project

---
+++ src/RentCarManagement.Client/Contracts/IHttpRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Client.Contracts;
+
+public interface IHttpRepository<T> where T : class
+{
+    Task<T> Get(string url, int id);
+    Task<T> GetDetails(string url, int id);
+    Task<List<T>> GetAll(string url);
+    Task Create(string url, T obj);
+    Task Update(string url, T obj, int id);
+    Task Delete(string url, int id);
+}
