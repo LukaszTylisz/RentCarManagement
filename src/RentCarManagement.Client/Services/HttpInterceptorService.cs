ea3be4cc5f4efcf86919d1e7dbed7ba78e995743
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-05-22 12:43:46 +0200

    Added Services

---
+++ src/RentCarManagement.Client/Services/HttpInterceptorService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor;
+
+namespace Client.Services;
+
+public class HttpInterceptorService(HttpClientInterceptor interceptor, NavigationManager navManager)
+{
+    public void MonitorEvent() => interceptor.AfterSend += InterceptResponse;
+
+    private void InterceptResponse(object sender, HttpClientInterceptorEventArgs e)
+    {
+        string message = string.Empty;
+
+        if (e.Response.IsSuccessStatusCode)
+        {
+            var responseCode = e.Response.StatusCode;
+
+            switch (responseCode)
+            {
+                case HttpStatusCode.NotFound:
+                    navManager.NavigateTo("/404");
+                    message = "The requested resource not found";
+                    break;
+                case HttpStatusCode.Unauthorized:
+                case HttpStatusCode.Forbidden:
+                    navManager.NavigateTo("/unauthorized");
+                    message = "You are not authorized to access this resource";
+                    break;
+                default:
+                    navManager.NavigateTo("/500");
+                    message = "Something went wrong, please contact with Administrator";
+                    break;
+            }
+
+            throw new HttpRequestException(message);
+        }
+    }
+
+    public void DisposeEvent() => interceptor.AfterSend -= InterceptResponse;
+}
---
+++ src/RentCarManagement.Client/Services/HttpRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http.Json;
+using Client.Contracts;
+
+namespace Client.Services;
+
+public class HttpRepository<T>(HttpClient client, HttpInterceptorService interceptor) : IDisposable, IHttpRepository<T>
+    where T : class
+{
+    public void Dispose()
+    {
+      interceptor.DisposeEvent();
+    }
+    
+    public async Task<T> Get(string url, int id)
+    {
+        interceptor.MonitorEvent();
+        return await client.GetFromJsonAsync<T>($"{url}/{id}");
+    }
+
+    public async Task<T> GetDetails(string url, int id)
+    {
+       interceptor.MonitorEvent();
+       return await client.GetFromJsonAsync<T>($"{url}/{id}/details");
+    }
+
+    public async Task<List<T>> GetAll(string url)
+    {
+        interceptor.MonitorEvent();
+        return await client.GetFromJsonAsync<List<T>>($"{url}");
+    }
+
+    public async Task Create(string url, T obj)
+    {
+        interceptor.MonitorEvent();
+        await client.PostAsJsonAsync(url, obj);
+    }
+
+    public async Task Update(string url, T obj, int id)
+    {
+        interceptor.MonitorEvent();
+        await client.PutAsJsonAsync($"{url}/{id}", obj);
+    }
+
+    public async Task Delete(string url, int id)
+    {
+        interceptor.MonitorEvent();
+        await client.DeleteAsync($"{url}/{id}");
+    }
+}
