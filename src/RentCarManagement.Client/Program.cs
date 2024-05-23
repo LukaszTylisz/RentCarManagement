19ff728c80f8f8be53a93d924c149b5e936d75f6
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-05-23 11:21:23 +0200

    Creating components on Client site in Razor

--- src/RentCarManagement.Client/Program.cs
+++ src/RentCarManagement.Client/Program.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Client.Contracts;
+using Client.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("RentCarManagement.Server", (sp, client) =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    client.EnableIntercept(sp);
+}).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RentCarManagement.Server"));
+
+builder.Services.AddHttpClientInterceptor();
+builder.Services.AddScoped<HttpInterceptorService>();
+builder.Services.AddTransient(typeof(IHttpRepository<>), typeof(HttpRepository<>));
+builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
