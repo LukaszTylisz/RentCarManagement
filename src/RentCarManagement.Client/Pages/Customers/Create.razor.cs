5278a32f0d9b169b2853d88650ab3ec9c855ba54
LukaszTylisz <lukasz.tylisz@gmail.com>
2024-06-05 14:42:49 +0200

    Adding Customers views

---
+++ src/RentCarManagement.Client/Pages/Customers/Create.razor.cs
@@ -0,0 +1,21 @@
+﻿using Client.Contracts;
+using Client.Services;
+using Client.Static;
+using Microsoft.AspNetCore.Components;
+using Shared.Domain;
+
+namespace RentCarManagement.Client.Pages.Customers;
+
+public partial class Create
+{
+    [Inject] private IHttpRepository<Customer> _client { get; set; }
+    [Inject] private NavigationManager _navManager { get; set; }
+    [Inject] private HttpInterceptorService _interceptor { get; set; }
+    private Customer customer = new Customer();
+
+    private async Task CreateCustomer()
+    {
+        await _client.Create(Endpoints.CustomersEndpoint, customer);
+        _navManager.NavigateTo("/customers/");
+    }
+}
